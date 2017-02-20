e66cef542b468107b58307f42d103042679deeb0
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2017-02-20 13:39:23 +0100

    Resolve exo3

--- app/src/main/java/fr/android/androidexercises/BookActivity.java
+++ app/src/main/java/fr/android/androidexercises/BookActivity.java
@@ -2,6 +2,7 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class BookActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class BookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+
+        String toastValue = R.string.toast_done + " " + getIntent().getStringExtra("name");
+        Toast.makeText(this, toastValue, Toast.LENGTH_SHORT).show();
     }
 }
