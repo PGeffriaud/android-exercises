3dbd920b94a0110f5e6785cebf226dde8ac1f53b
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2017-02-20 18:30:43 +0100

    Resolve Exo9 with image

--- app/src/main/java/fr/android/androidexercises/BookItemView.java
+++ app/src/main/java/fr/android/androidexercises/BookItemView.java
@@ -2,6 +2,7 @@ package fr.android.androidexercises;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -9,6 +10,7 @@ public class BookItemView extends LinearLayout {
 
     private TextView nameTextView;
     private TextView priceTextView;
+    private ImageView imageView;
 
     public BookItemView(Context context) {
         this(context, null);
@@ -27,12 +29,13 @@ public class BookItemView extends LinearLayout {
         super.onFinishInflate();
         this.nameTextView = (TextView) findViewById(R.id.nameTextView);
         this.priceTextView = (TextView) findViewById(R.id.priceTextView);
+        this.imageView = (ImageView) findViewById(R.id.imageView);
     }
 
     public void bindView(Book book) {
         this.nameTextView.setText(book.name);
         this.priceTextView.setText(String.valueOf(book.price));
-
+        this.imageView.setImageResource(R.drawable.ic_launcher);
 
     }
 }
