f3348ab3e2960de0f608ff7c0b5b7b2ee198fe51
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2017-02-21 12:45:28 +0100

    Resolve Exo12

---
+++ app/src/test/java/fr/android/androidexercises/LoginPresenterTest.java
@@ -0,0 +1,54 @@
+package fr.android.androidexercises;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by PierreG on 21/02/17.
+ *
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoginPresenterTest {
+
+    @Mock
+    private LoginActivity activity;
+
+    @InjectMocks
+    private LoginPresenter presenter;
+
+    @Test
+    public void shouldLoginIfValidPassword() throws Exception {
+        String password = "password";
+        presenter.checkCredentials(password);
+
+        Mockito.verify(activity).logged();
+        Mockito.verify(activity).message(R.string.text_logged);
+    }
+
+    @Test
+    public void shouldNotLoginIfNullPassword() throws Exception {
+        String password = null;
+        presenter.checkCredentials(password);
+
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
+
+    @Test
+    public void shouldNotLoginIfSmallPassword() throws Exception {
+        String password = "pa";
+        presenter.checkCredentials(password);
+
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
+
+}
